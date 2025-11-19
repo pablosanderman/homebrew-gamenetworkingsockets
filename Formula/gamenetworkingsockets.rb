21c3238394ce462808bb1e67730fd031f8e352a8
Pablo Sanderman <pablosanderman@gmail.com>
2025-11-19 11:07:49 +0100

    Add GameNetworkingSockets 1.4.1 formula

---
+++ Formula/gamenetworkingsockets.rb
@@ -0,0 +1,54 @@
+class Gamenetworkingsockets < Formula
+  desc "Valve's transport-layer networking library with NAT traversal"
+  homepage "https://github.com/ValveSoftware/GameNetworkingSockets"
+  url "https://github.com/ValveSoftware/GameNetworkingSockets/archive/refs/tags/v1.4.1.tar.gz"
+  sha256 "1cfb2bf79c51a08ae4e8b7ff5e9c1266b43cfff6f53ecd3e7bc5e3fcb2a22503"
+  license "BSD-3-Clause"
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "openssl@3"
+  depends_on "protobuf"
+
+  def install
+    args = %W[
+      -GNinja
+      -DCMAKE_BUILD_TYPE=Release
+      -DUSE_CRYPTO=OpenSSL
+      -DBUILD_TESTS=OFF
+      -DBUILD_EXAMPLES=OFF
+      -DBUILD_TOOLS=OFF
+      -DENABLE_ICE=ON
+      -DOPENSSL_ROOT_DIR=#{Formula["openssl@3"].opt_prefix}
+      -DProtobuf_PROTOC_EXECUTABLE=#{Formula["protobuf"].opt_bin}/protoc
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    pkgshare.install Dir["build/lib/cmake/GameNetworkingSockets"]
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~CMAKE
+      cmake_minimum_required(VERSION 3.16)
+      project(gns_test LANGUAGES CXX)
+      find_package(GameNetworkingSockets REQUIRED)
+      add_executable(gns_test main.cpp)
+      target_link_libraries(gns_test PRIVATE GameNetworkingSockets::GameNetworkingSockets)
+    CMAKE
+
+    (testpath/"main.cpp").write <<~CPP
+      #include <steam/steamnetworkingsockets.h>
+      int main() {
+        SteamNetworkingIdentity id;
+        id.Clear();
+        return k_ESteamNetworkingSocketsDebugOutputType_None;
+      }
+    CPP
+
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_PREFIX_PATH=#{prefix}"
+    system "cmake", "--build", "build"
+  end
+end
